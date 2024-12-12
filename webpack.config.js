69280ffe28419f8aee55747d400cc7f31b9423a8
BiigToastie <ihre.email@example.com>
2024-12-12 08:18:35 +0100

    perf: Optimiere Bundle-Größe und Webpack-Konfiguration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -7,6 +8,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public/dist'),
+        clean: true
     },
     module: {
         rules: [
@@ -23,6 +25,51 @@ module.exports = {
             'three': path.resolve(__dirname, 'node_modules/three')
         }
     },
-    mode: 'development',
-    devtool: 'source-map'
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                    compress: {
+                        drop_console: true,
+                        drop_debugger: true
+                    }
+                },
+                extractComments: false,
+            }),
+        ],
+        splitChunks: {
+            chunks: 'all',
+            minSize: 20000,
+            maxSize: 244000,
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                    priority: -10
+                },
+                threejs: {
+                    test: /[\\/]node_modules[\\/]three[\\/]/,
+                    name: 'threejs',
+                    chunks: 'all',
+                    priority: 20
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true
+                }
+            }
+        }
+    },
+    mode: 'production',
+    performance: {
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
+        hints: 'warning'
+    }
 }; 
